6c1d7ac7888b538a4c33e3a820b42c98bae790f2
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-06-09 15:30:59 +0530

    building the todo app with react-redux

---
+++ 11_Redux_Toolkit_Todo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import AddTodos from "./components/AddTodos";
+import Todos from "./components/Todos";
+function App() {
+  return (
+    <>
+      <AddTodos />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
