8e0d142f2ff436618cbc64529d84ce54ceac0aa3
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-07-05 01:43:59 +0530

    setting up redux-toolkit

--- 12_Mega_Blog_Project/src/App.jsx
+++ 12_Mega_Blog_Project/src/App.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
-
+import { Header, Footer } from "./components/index.js";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>This is a blog app build with react</h1>
-    </>
-  );
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block text-center">
+        <Header />
+        <main>
+         TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
